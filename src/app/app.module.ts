139a5decc79c09328396c66a7dfac791f63ba67b
ans49 <dev.galima@outlook.fr>
2024-06-19 21:48:32 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DiagramComponent } from './diagram/diagram.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiagramComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
